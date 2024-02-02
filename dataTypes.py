d59e4a92785e856f2cf2335d6a6373798d203c0a
alx3416 <alx3416@hotmail.com>
2024-02-02 09:29:40 -0600

    ejemplos tipos de dato simples

---
+++ dataTypes.py
@@ -0,0 +1,23 @@
+
+# Soy un comentario!
+print("Hello, World!")
+
+# data types
+
+# string
+myString = "Hello, World!"
+print(myString)
+
+# numeric
+myNumber = 5
+print(type(myNumber))
+print(myNumber)
+
+myFloatNumber = 3.1416
+print(type(myFloatNumber))
+print(myFloatNumber)
+
+myComplexNumber = complex(2.718, 3.1416)
+print(type(myComplexNumber))
+print("real value: " + str(myComplexNumber.real))
+print("imaginary value: " + str(myComplexNumber.imag))

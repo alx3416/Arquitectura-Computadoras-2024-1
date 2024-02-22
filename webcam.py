b17365289ca5ba38f6b83c7a414b2dce3076c35d
alx3416 <63263335+alx3416@users.noreply.github.com>
2024-02-22 16:56:03 -0600

    Update webcam.py

--- webcam.py
+++ webcam.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 kernel = np.array([-1, 1])
-cam = cv2.VideoCapture(1)
+cam = cv2.VideoCapture(0)
 while True:
     ret_val, img = cam.read()
 
@@ -10,8 +10,13 @@ while True:
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     rows, cols, channels = img.shape
     img_out = img_gray.copy()
+    for row in range(rows-1):
+        for col in range(cols):
+            img_out[row][col] = float(img_gray[row][col]) - float(img_gray[row + 1][col])
+            img_out[row][col] =np.uint8((img_out[row][col] + 255)/2)
 
     cv2.imshow('my webcam', img_out)
+    cv2.imshow('original', img)
     if cv2.waitKey(1) == 27: 
         break  # esc to quit
 cv2.destroyAllWindows()

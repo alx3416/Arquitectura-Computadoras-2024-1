10a29674d85cbd8c2d54c83dd4451ab5cd28d2f0
alx3416 <63263335+alx3416@users.noreply.github.com>
2024-02-29 14:51:39 -0600

    Update ex2.py

--- ex2.py
+++ ex2.py
@@ -1,13 +1,17 @@
 # Escriba un programa que obtenga el primer y ultimo valor de una lista
 def getFirstAndLastFrom(myList):
-    return myList
+    return myList[0], myList[-1]
 
 
 def getFibonacciNumbers(value):
+    fibonacciNumbers = [1, 1]
+    newValue = 2
+    while newValue <= value:
+        newValue = fibonacciNumbers[-2] + fibonacciNumbers[-1]
+        if newValue >= value:
+            break
+        fibonacciNumbers.append(newValue)
+    return fibonacciNumbers
 
-    return value
-
-
-
-result = getFirstAndLastFrom([1, 2, 3, 4, 5])
+result = getFibonacciNumbers(0)
 print(result)

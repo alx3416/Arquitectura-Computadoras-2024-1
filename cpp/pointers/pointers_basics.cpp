387a4cc4a239af4dbc072667be27bb430e477d28
alx3416 <63263335+alx3416@users.noreply.github.com>
2024-04-30 15:24:19 -0600

    adding pointers example

---
+++ cpp/pointers/pointers_basics.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main() {
+    int myAge = 43;
+    int* ptr = &myAge;
+
+    printf("%d\n", myAge);
+    printf("%p\n", &myAge);
+    printf("%p\n", ptr);
+
+    char* myWord = "Hello";
+    char myWord2[] = "Hello";
+
+    printf("Second char is: %c\n", myWord[1]);
+    printf("Second char is: %c\n", *(myWord+1));
+
+    printf("Second char is: %s\n", (myWord+1));
+
+
+
+    return 0;
+
+}

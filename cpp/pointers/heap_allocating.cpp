c530da2cbccdc06aa7b1fdbe9ffd7307b9d6b8b5
Victor Alejandro Gonzalez Huitron <victor.gh@queretaro.tecnm.mx>
2024-05-07 00:42:06 -0600

    adding memory allocation

---
+++ cpp/pointers/heap_allocating.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <stdlib.h>
+
+// check https://www.geeksforgeeks.org/dynamic-memory-allocation-in-c-using-malloc-calloc-free-and-realloc/
+
+int main()
+{
+    // C malloc()
+    // memory allocation, regresa un puntero del tipo void, que puede ser casteado a otro tipo de dato
+    int* ptrLargeArray = (int*)malloc(4000000 * sizeof(int));
+
+    // ejemplo usando dos instrucciones en lugar de una
+    void* ptr = malloc(4000000 * sizeof(int));
+    auto ptr_int = (int*)ptr;
+
+    // Podemos usar C++ casting
+    auto ptrLargeArray2 = static_cast<int*>(malloc(4000000 * sizeof(int)));
+
+    std::cout << "position 123,456: " << ptrLargeArray[123456] << " valor al crear arreglo \n";
+    for (size_t j = 0; j < 4000000; j++)
+    {
+        ptrLargeArray[j] = 7;
+    }
+
+    std::cout << "position 123,456: " << ptrLargeArray[123456] << " Press any key to continue \n";
+    std::cin.ignore(); // revisamos uso de RAM
+
+    // Es responsabilidad del desarrollador liberar memoria después de usarse
+    free(ptrLargeArray);
+    free(ptr_int);
+
+    // C calloc
+    // contiguous allocation, la diferencia es que los valores del arreglo por default son cero al inicio
+    // se pasan dos argumentos de entrada, numero de elementos  y su tamaño
+
+    auto myArray = (int*)calloc(4000000, sizeof(int));
+
+    std::cout << "position 123,456: " << myArray[123456] << " valor al crear arreglo \n";
+    for (size_t j = 0; j < 4000000; j++)
+    {
+        myArray[j] = 121212;
+    }
+
+    std::cout << "position 123,456: " << myArray[123456] << " Press any key to continue \n";
+    std::cin.ignore(); // revisamos uso de RAM
+
+    // C realloc
+    // modifica el tamaño del array, en caso de añadir valores, estos se inician en cero
+    myArray = (int*)realloc(myArray, 10);
+    for (size_t j = 0; j < 10; j++)
+    {
+        myArray[j] = 3;
+    }
+    for (size_t j = 0; j < 12; j++)
+    {
+        std::cout << "position " << j << " valor guardado es " << myArray[j] << std::endl;
+    }
+    std::cin.ignore(); // revisamos uso de RAM
+    free(myArray);
+
+    return 0;
+}
---
+++ cpp/pointers/heap_allocating_new.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stdlib.h>
+
+// check https://www.geeksforgeeks.org/dynamic-memory-allocation-in-c-using-malloc-calloc-free-and-realloc/
+
+int main()
+{
+    //  C++ dynamic memory
+    //  Para usar HEAP usamos new, para borrar, delete
+    size_t n = 10;
+    int *data = new int[n];
+
+    for (size_t j = 0; j < n; j++)
+    {
+        std::cout << "position " << j << " valor guardado es " << data[j] << std::endl;
+    }
+
+    for (size_t j = 0; j < n; j++)
+    {
+        data[j] = 11;
+    }
+
+    std::cout << "position " << 3 << " valor guardado es " << data[3] << std::endl;
+    std::cin.ignore(); // revisamos uso de RAM
+
+    delete[] data;
+
+    return 0;
+}

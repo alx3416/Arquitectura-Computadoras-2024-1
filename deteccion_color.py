fc933b728ed31006619539528d65fc228e4ddd0c
alx3416 <63263335+alx3416@users.noreply.github.com>
2024-11-04 13:49:48 -0600

    deteccion color demo

---
+++ deteccion_color.py
@@ -0,0 +1,38 @@
+# Para usar funciones en otro programa, hay que agregar definición al inicio
+# from <archivo.py> import <mifuncion> # Ejemplo para importar una función en archivo py
+
+def deteccion_color(img_in):  # Nombre de la función y variables de entrada
+    # Librerias a usar en esta función
+    import numpy as np
+    import cv2
+
+    img_in = cv2.cvtColor(img_in, cv2.COLOR_BGR2LAB)
+
+    L = img_in[:, :, 0]
+    a = img_in[:, :, 1]  # Tomamos canal a (verdes negativo (0-127) y rojos positivo (128-25))
+    b = img_in[:, :, 2]  # tomamos canal b (positivos amarillo, negativos azules)
+    # L = np.double(L) # Conversión tipo de dato
+    a = np.double(a)
+    b = np.double(b)
+    a = a - 128
+    b = b - 128
+
+    # z=(np.bitwise_and(a>0,a>np.abs(b))) # Detección de rojos
+    # z=np.bitwise_and(a<0,np.abs(a)>np.abs(b)) # Detección de verdes
+    # z=np.bitwise_and( b>0, b>np.abs(a) ) # Detección de amarillos
+    z = np.bitwise_and(b < 0, np.abs(b) > np.abs(a))  # Detección de azules
+
+    new_a = a
+    new_b = b
+    new_a[z == 1] = b[z == 1]
+    new_b[z == 1] = a[z == 1] * (-1)
+
+    img_in[:, :, 0] = L
+    img_in[:, :, 1] = new_a + 128
+    img_in[:, :, 2] = new_b + 128
+
+    img_out = np.uint8(img_in)
+    img_out = cv2.cvtColor(img_out, cv2.COLOR_LAB2BGR)
+
+    # variables de salida
+    return img_out;
---
+++ webcam_deteccion_color.py
@@ -0,0 +1,26 @@
+import numpy as np
+import cv2
+from deteccion_color import deteccion_color
+
+# from <archivo.py> import <mifuncion> # Ejemplo para importar una función en archivo py
+
+cap = cv2.VideoCapture(1)
+ret, frame = cap.read()
+img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+while (True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    Im2 = deteccion_color(frame)
+
+    # Display the resulting frame
+    cv2.imshow('Procesado', Im2)
+    cv2.imshow('Original', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()

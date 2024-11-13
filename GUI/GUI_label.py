80080bd0ed5f9722836093ccabd33e9577479bd2
alx3416 <63263335+alx3416@users.noreply.github.com>
2024-11-13 13:59:42 -0600

    adding GUI label examples

--- GUI/GUI_label.py
+++ GUI/GUI_label.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Nov  3 10:42:58 2020
-
-@author: Alejandro
-"""
-
 # Importamos módulo de tkinter, lo llamaremos tk dentro del código
 import tkinter as tk
 
@@ -18,23 +11,24 @@ greeting = tk.Label(text="Soy un label widget!")
 
 # leemos imagen
 logo = tk.PhotoImage(file="pythonlogo.png")
-# ponemos imagen en una etiqueta en lugar de poner texto
-w1 = tk.Label(window, image=logo).pack()
+# ponemos la imagen en la etiqueta
+w1 = tk.Label(window, image=logo)
+w1.pack()
 
 # etiqueta con colores personalizados
 label = tk.Label(
     text="Soy una etiqueta!",
     foreground="yellow",
-    background="#555555", # color puede escribirse en RGB, como hexadecimal
+    background="#555555",  # color puede escribirse en RGB, como hexadecimal
     width=15,
-    height=15 # la unidad es el ancho y alto del caracter 0
-).pack()
+    height=15  # la unidad es el ancho y alto del caracter 0
+)
+label.pack()
 # lista de colores disponibles
-# http://www.science.smith.edu/dftwiki/index.php/Color_Charts_for_TKinter
+# http://dominiquethiebaut.com/dftwiki3/images/3/3d/TkInterColorCharts.png
 
 
 greeting.pack()
-
 
 # Al final del código añadimos mainloop, asi nuestra GUI estará esperando acción del usuario
 # esta espera es un ciclo llamado "event loop", también bloqueará python para ejecutar otro codigo
